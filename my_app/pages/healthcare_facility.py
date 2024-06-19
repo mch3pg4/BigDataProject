f37a6b554e58e5fe5699633244f3e8e45432b4bd
Zi Hao Wong <111846088+Wong86-cloud@users.noreply.github.com>
2024-06-19 11:19:57 +0800

    Enhance the websites

---
+++ my_app/pages/healthcare_facility.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title('Healthcare Facility Page')
+    st.write('Welcome to the healthcare facility page.')
+
+if __name__ == "__main__":
+    main()
+
---
+++ my_app/pages/mental_health.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title('Mental Health Page')
+    st.write('Welcome to the mental health page.')
+
+if __name__ == "__main__":
+    main()
