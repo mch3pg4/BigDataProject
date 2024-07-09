4103d15f06c291bedad734574e3971050e481c7f
Zi Hao Wong <111846088+Wong86-cloud@users.noreply.github.com>
2024-07-09 17:25:04 +0800

    modified the code

--- my_app/pages/1_💭_Mental_Health.py
+++ my_app/pages/1_💭_Mental_Health.py
@@ -51,9 +51,9 @@ def main():
        """)
 
     # Directory containing images and metadata file
-    image_dir = '../images/MH_graphs'
-    html_dir = '../images/MH_graphs/html'
-    metadata_file = os.path.join(image_dir, 'metadata.csv')
+    image_dir = 'images/MH_graphs'
+    html_dir = 'images/MH_graphs/html'
+    metadata_file = 'images/MH_graphs/metadata.csv'
 
     if not os.path.exists(metadata_file):
         st.error("Metadata file not found!")
